4959ae8b473b79925c2e7ec282c0969f5e0f4cdf
Madars Bitenieks <madza@madzathemes.com>
2018-10-24 04:49:52 +0300

    fix

--- inc/functions/functions-hooks.php
+++ inc/functions/functions-hooks.php
@@ -340,6 +340,20 @@ function xnews_css() {
 }
 add_action( 'wp_enqueue_scripts', 'xnews_css');
 
+
+add_action( 'wp_default_scripts', 'move_jquery_into_footer' );
+
+function move_jquery_into_footer( $wp_scripts ) {
+
+    if( is_admin() ) {
+        return;
+    }
+
+    $wp_scripts->add_data( 'jquery', 'group', 1 );
+    $wp_scripts->add_data( 'jquery-core', 'group', 1 );
+    $wp_scripts->add_data( 'jquery-migrate', 'group', 1 );
+}
+
 function xnews_header_script() {
 
 		$option = get_option("xnews_theme_options");
